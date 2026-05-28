5b70fdece2a327b0fe56b7ac2d0177e18da09bdd
shaxriyorcerebry-hash <shaxriyorcerebry@gmail.com>
2026-05-28 21:42:57 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:basic_app/Gradient_Container.dart';
+
+
+
+void main(){
+  
+  runApp(
+    MaterialApp(
+home:Scaffold(
+  body:  GradientContainer(),
+)
+    )
+  );
+}
